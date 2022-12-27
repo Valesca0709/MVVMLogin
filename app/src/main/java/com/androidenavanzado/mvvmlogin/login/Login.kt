dc4297a0ce8b653433669dcced46244d04a34541
Valesca <valesca.torres07@gmail.com>
2022-12-27 12:58:45 -0300

    Se borrò variable lazy de firebase

--- app/src/main/java/com/androidenavanzado/mvvmlogin/login/Login.kt
+++ app/src/main/java/com/androidenavanzado/mvvmlogin/login/Login.kt
@@ -23,12 +23,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
-private val auth by lazy {
-    Firebase.auth
-}
-
-
-
 @Composable
 fun LoginScreen(onClickLogin: () -> Unit) {
     Box(
